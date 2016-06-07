227a4698fedc3ae7d89fbed5f743c408b6eb3411
Viel Eclarinal <eclarinalviel@gmail.com>
2016-06-07 20:57:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<div class="row text-center no-margin">
 		<?php 
 		$currentPage = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		$args = array('posts_per_page' => 4, 'paged' => $currentPage);
+		$args = array('posts_per_page' => 3, 'paged' => $currentPage);
 		query_posts($args);	
 		if( have_posts() ): $i = 0;
 			
